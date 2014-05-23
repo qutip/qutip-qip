b650f2aa32543f1ce86ea546c79c3b13d62b37a4
Anubhav Vardhan <anubhavvardhan@gmail.com>
2014-05-23 15:37:25 +0900

    Renamed quantum_info module to qip

--- src/qutip_qip/utils.py
+++ src/qutip_qip/utils.py
@@ -30,54 +30,28 @@
 #    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
-
-import os
-from numpy.testing import assert_, run_module_suite
-from numpy.linalg import norm
-
-from qutip import *
-from qutip.qip.gates import *
+import numpy as np
 
 
-def test_qpt_snot():
-    "quantum process tomography for snot gate"
-
-    U_psi = snot()
-    U_rho = spre(U_psi) * spost(U_psi.dag())
-    N = 1
-    op_basis = [[qeye(2), sigmax(), 1j * sigmay(), sigmaz()] for i in range(N)]
-    op_label = [["i", "x", "y", "z"] for i in range(N)]
-    chi1 = qpt(U_rho, op_basis)
-
-    chi2 = zeros((2 ** (2 * N), 2 ** (2 * N)), dtype=complex)
-    chi2[1, 1] = chi2[1, 3] = chi2[3, 1] = chi2[3, 3] = 0.5
-
-    assert_(norm(chi2 - chi1) < 1e-8)
-
-
-def test_qpt_cnot():
-    "quantum process tomography for cnot gate"
-
-    U_psi = cnot()
-    U_rho = spre(U_psi) * spost(U_psi.dag())
-    N = 2
-    op_basis = [[qeye(2), sigmax(), 1j * sigmay(), sigmaz()] for i in range(N)]
-    op_label = [["i", "x", "y", "z"] for i in range(N)]
-    chi1 = qpt(U_rho, op_basis)
-
-    chi2 = zeros((2 ** (2 * N), 2 ** (2 * N)), dtype=complex)
-    chi2[0, 0] = chi2[0, 1] = chi2[1, 0] = chi2[1, 1] = 0.25
-
-    chi2[12, 0] = chi2[12, 1] = 0.25
-    chi2[13, 0] = chi2[13, 1] = -0.25
-
-    chi2[0, 12] = chi2[1, 12] = 0.25
-    chi2[0, 13] = chi2[1, 13] = -0.25
-
-    chi2[12, 12] = chi2[13, 13] = 0.25
-    chi2[13, 12] = chi2[12, 13] = -0.25
-
-    assert_(norm(chi2 - chi1) < 1e-8)
-
-if __name__ == "__main__":
-    run_module_suite()
+def _reg_str2array(string,width):
+    """
+    Takes a string of 'u','d','0',and '1' and returns
+    a flattened array of 0's and 1's.
+    
+    Parameters
+    ----------
+    string: str
+        Input string
+    
+    """
+    if len(string)!=width:
+        raise ValueError('Registers string length is not equal to the registers width.')
+    mask=[]
+    for kk in range(len(string)):
+        if string[kk] in ['u','0']:
+            mask+=[0]
+        elif string[kk] in ['d','1']:
+            mask+=[1]
+        else:
+            raise ValueError("String element '"+string[kk]+"' is not a valid label.")
+    return np.array(mask)
