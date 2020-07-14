aa6d5eb2a5db36b53c9a534cbab9a6dbe897149a
sid <ssaraogi@edu.uwaterloo.ca>
2020-07-13 23:40:50 -0400

    initial qutip_qip commit

---
+++ qip/version.py
@@ -0,0 +1,4 @@
+# THIS FILE IS GENERATED FROM QUTIP SETUP.PY
+short_version = '0.0.1'
+version = '0.0.1.dev1+bb0c2d9'
+release = False
---
+++ test_metrics.py
@@ -0,0 +1,23 @@
+*~
+*.py[cod]
+*.so
+.DS_Store
+.f2py_f2cmap
+.idea/
+
+# Packages
+*.egg
+*.egg-info
+dist
+build
+eggs
+parts
+bin
+var
+sdist
+develop-eggs
+.installed.cfg
+lib
+lib64
+
+qip/version.py
