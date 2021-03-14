7a3330c2a6b39f987a2fc1efdf293d4ca2999349
Boxi Li <etamin1201@gmail.com>
2021-03-14 12:35:21 +0100

    Copy some confs from qutip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,7 +30,16 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.mathjax',
+              'matplotlib.sphinxext.plot_directive',
+              'sphinx.ext.autodoc',
+              'sphinx.ext.doctest',
+              'sphinx.ext.autosummary',
+              'numpydoc',
+              'sphinx.ext.extlinks',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.ifconfig',
+              'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +62,87 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for LaTeX output ---------------------------------------------
+
+# AJGP 2017-01-8: Switching to manual, ditching the preamble
+
+# latex_header = open('latex_output_files/latex_preamble.tex', 'r+')
+# PREAMBLE = latex_header.read();
+
+latex_elements = {
+                  'papersize':'a4paper',
+                  'pointsize':'10pt',
+                  'classoptions': '',
+                  'babel': '\\usepackage[english]{babel}',
+                  'fncychap' : '',
+                  'figure_align': 'H',
+#                  'preamble': PREAMBLE
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+  ('index', 'qutip.tex', project, author, 'manual'),
+]
+
+# The name of an image file (relative to this directory) to place at the top of
+# the title page.
+latex_logo = 'figures/logo.png'
+
+# Sometimes make might suggest setting this to False.
+# It screws a few things up if you do - don't be tempted.
+latex_keep_old_macro_names=True
+
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = True
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL addresses after external links.
+#latex_show_urls = False
+
+# Documents to append as an appendix to all manuals.
+#latex_appendices = []
+
+# If false, no module index is generated.
+#latex_domain_indices = True
+
+# -- Doctest Setup ---------------------------------------
+
+os_nt = False
+if os.name == "nt":
+    os_nt = True
+
+doctest_global_setup = '''
+from pylab import *
+from qutip import *
+import numpy as np
+import os
+import warnings
+warnings.filterwarnings("ignore")
+os_nt = {}
+'''.format(os_nt)
+
+# -- Options for plot directive ---------------------------------------
+
+plot_working_directory = "./"
+plot_pre_code = """
+from pylab import *
+from scipy import *
+from qutip import *
+import numpy as np
+import matplotlib.pyplot as plt
+plt.close("all")
+"""
+plot_include_source = True
+plot_html_show_source_link = False
+
+# -- Options for numpydoc ---------------------------------------
+
+numpydoc_show_class_members = False
+napoleon_numpy_docstring = True
+napoleon_use_admonition_for_notes = True
