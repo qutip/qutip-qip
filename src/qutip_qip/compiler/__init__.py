b3cf5932f9dfb8ee2d666b684587f9387648a8ac
Boxi Li <etamin1201@gmail.com>
2021-05-01 15:09:28 +0200

    create circuit qed model

--- src/qutip_qip/compiler/__init__.py
+++ src/qutip_qip/compiler/__init__.py
@@ -36,3 +36,4 @@ from .scheduler import Scheduler
 from .gatecompiler import GateCompiler
 from .cavityqedcompiler import CavityQEDCompiler
 from .spinchaincompiler import SpinChainCompiler
+from .circuitqedcompiler import TransmonChainCompiler
