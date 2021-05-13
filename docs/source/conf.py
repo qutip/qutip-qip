d923b268fa4d37f6fb117e6c7e421a8337442a76
Boxi Li <etamin1201@gmail.com>
2021-05-13 11:37:46 +0200

    Fix the doc version.
    The doc version was wrongly set to 0.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, QuTiP Community'
 author = 'QuTiP Community'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
