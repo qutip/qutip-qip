d8f125de4ee4d9c15283af64adc6abd5dde18846
Boxi Li <etamin1201@gmail.com>
2021-05-15 09:48:28 +0200

    Bump version to 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, QuTiP Community'
 author = 'QuTiP Community'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.0dev'
 
 
 # -- General configuration ---------------------------------------------------
