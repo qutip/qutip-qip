2dc3b411d0f8af8ec1f846890f5f7346d31ca796
Boxi Li <etamin1201@gmail.com>
2021-05-18 23:01:17 +0200

    revert version change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, QuTiP Community'
 author = 'QuTiP Community'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0dev'
+release = '0.1.0dev'
 
 
 # -- General configuration ---------------------------------------------------
