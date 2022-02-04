07cc5a4539c4aa05e14e5b1afc344dc10e87968d
purva-thakre <purva.athakre@gmail.com>
2022-02-04 10:02:47 -0600

    tests for split partial

--- tests/decomposition_functions/test_general_decomposition.py
+++ tests/decomposition_functions/test_general_decomposition.py
@@ -8,6 +8,7 @@ from qutip_qip.decompose.decompose_general_qubit_gate import (
     _decompose_to_two_level_arrays,
     _create_dict_for_two_level_arrays,
     _partial_gray_code,
+    _split_partial_gray_code,
 )
 
 
@@ -135,3 +136,133 @@ def test_three_qubit_partial_grey_code_output():
     }
     assert func_output == expected_output
 
+
+@pytest.mark.parametrize("num_qubits", [2, 3, 4, 5])
+def test_len_split_grey_code(num_qubits):
+    """Check if length of both dict outputs of split gray code is equal to
+    the total number of two-level array gates.
+
+    First dict is made up of n-bit toffoli and second is made up of
+    gate that needs decomposition.
+    """
+    input_gate = rand_unitary(2 ** num_qubits, dims=[[2] * num_qubits] * 2)
+    array_decompose = _decompose_to_two_level_arrays(
+        input_gate, num_qubits, expand=False
+    )
+    len_two_level_array = len(array_decompose)
+    func_out = _split_partial_gray_code(
+        _partial_gray_code(num_qubits, array_decompose)
+    )
+
+    # Checks if 2 separate dictionaries are always returned
+    assert len(func_out) == 2
+
+    # check length of each dictionary
+    assert len(func_out[0]) == len(array_decompose)
+    assert len(func_out[1]) == len(array_decompose)
+
+
+def test_two_qubit_split_partial_grey_code():
+    """Checks if output of split partial gray code function is correct
+    with expected output for two qubits."""
+    num_qubits = 2
+    input_gate = rand_unitary(2 ** num_qubits, dims=[[2] * num_qubits] * 2)
+    array_decompose = _decompose_to_two_level_arrays(
+        input_gate, num_qubits, expand=False
+    )
+    split_output = _split_partial_gray_code(
+        _partial_gray_code(num_qubits, array_decompose)
+    )
+    expected_toffoli_output = {
+        6: None,
+        5: None,
+        4: ["01"],
+        3: ["00"],
+        2: ["00", "01"],
+        1: None,
+    }
+    expected_gate_decom_out = {
+        6: ["11", "10"],
+        5: ["01", "11"],
+        4: ["10", "11"],
+        3: ["11", "01"],
+        2: ["10", "11"],
+        1: ["00", "01"],
+    }
+    assert expected_toffoli_output == split_output[0]
+    assert expected_gate_decom_out == split_output[1]
+
+
+def test_three_qubit_split_partial_grey_code():
+    """Checks if output of split partial gray code function is correct
+    with expected output for three qubits."""
+    num_qubits = 3
+    input_gate = rand_unitary(2 ** num_qubits, dims=[[2] * num_qubits] * 2)
+    array_decompose = _decompose_to_two_level_arrays(
+        input_gate, num_qubits, expand=False
+    )
+    split_output = _split_partial_gray_code(
+        _partial_gray_code(num_qubits, array_decompose)
+    )
+    expected_toffoli_output = {
+        28: None,
+        27: None,
+        26: ["110"],
+        25: ["111"],
+        24: ["110", "111"],
+        23: None,
+        22: ["011", "010"],
+        21: ["011"],
+        20: ["011", "010", "110"],
+        19: ["011", "010", "110", "111"],
+        18: ["010"],
+        17: None,
+        16: ["010", "110"],
+        15: ["010", "110", "111"],
+        14: None,
+        13: ["001", "011", "010"],
+        12: ["001", "011"],
+        11: ["001", "011", "010", "110"],
+        10: ["001", "011", "010", "110", "111"],
+        9: None,
+        8: ["001"],
+        7: ["000", "001", "011", "010"],
+        6: ["000", "001", "011"],
+        5: ["000", "001", "011", "010", "110"],
+        4: ["000", "001", "011", "010", "110", "111"],
+        3: ["000"],
+        2: ["000", "001"],
+        1: None,
+    }
+    expected_gate_decom_out = {
+        28: ["110", "111"],
+        27: ["111", "101"],
+        26: ["101", "111"],
+        25: ["100", "101"],
+        24: ["100", "101"],
+        23: ["101", "100"],
+        22: ["111", "110"],
+        21: ["110", "010"],
+        20: ["101", "111"],
+        19: ["100", "101"],
+        18: ["111", "110"],
+        17: ["010", "110"],
+        16: ["101", "111"],
+        15: ["100", "101"],
+        14: ["011", "010"],
+        13: ["111", "110"],
+        12: ["110", "010"],
+        11: ["101", "111"],
+        10: ["100", "101"],
+        9: ["001", "011"],
+        8: ["010", "011"],
+        7: ["111", "110"],
+        6: ["110", "010"],
+        5: ["101", "111"],
+        4: ["100", "101"],
+        3: ["011", "001"],
+        2: ["010", "011"],
+        1: ["000", "001"],
+    }
+    assert expected_toffoli_output == split_output[0]
+    assert expected_gate_decom_out == split_output[1]
