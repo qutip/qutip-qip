5e51fdb40b9292d92580f8c176ff48ce0b5f920e
shreyas <shpradhan12@gmail.com>
2022-04-17 06:39:47 +0530

    added about function for qutip-qip

--- src/qutip_qip/__init__.py
+++ src/qutip_qip/__init__.py
@@ -1,1 +1,2 @@
 from .version import version as __version__  # noqa
+from .about import *
---
+++ src/qutip_qip/about.py
@@ -0,0 +1,18 @@
+"""
+Command line output of information on QuTiP QIP and dependencies.
+"""
+__all__ = ['about']
+
+import qutip
+
+
+def about():
+    """
+    About box for QuTiP QIP. Gives version numbers for QuTiP, QuTiP QIP, NumPy, SciPy, Cython,
+    and MatPlotLib.
+    """
+    qutip.about("qutip_qip")
+
+
+if __name__ == "__main__":
+    about()
