0e3da248153712266d3604acdbc3e072d7b3f361
christian512 <c.staufe@posteo.net>
2022-08-22 12:01:46 +0200

    fix usage of rand_ket

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -346,7 +346,10 @@ class Test_expand_operator:
             np.testing.assert_allclose(test.full(), expected.full())
 
 def test_gates_class():
-    init_state = qutip.rand_ket(8, dims=[[2, 2, 2], [1, 1, 1]])
+    if parse_version(qutip.__version__) < parse_version('5.dev'):
+        init_state = qutip.rand_ket(8, dims=[[2, 2, 2], [1, 1, 1]])
+    else:
+        init_state = qutip.rand_ket([2,2,2])
 
     circuit1 = QubitCircuit(3)
     circuit1.add_gate("X", 1)
