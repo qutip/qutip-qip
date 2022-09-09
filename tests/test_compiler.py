24f7632cec15898f411764585d0bad373ecd5ed1
BoxiLi <etamin1201@gmail.com>
2022-09-09 17:31:24 +0200

    Fix bugs in the analytical pulse shapes
    - Fix bugs in the analytical pulse shapes
    - Compress the test. The test no longer depends on the processor class. Test both the maximum and the total area.

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 from numpy.testing import assert_array_equal
+from scipy import integrate
 from qutip_qip.compiler.gatecompiler import _default_window_t_max
 
 from qutip_qip.device import (
@@ -201,22 +202,11 @@ def test_compiler_result_format():
 
 
 @pytest.mark.parametrize(
-    "shape", list(_default_window_t_max.keys()) + ["rectangular"])
-def test_pulse_shape_scipy(shape):
-    """Test different pulse shape functions imported from scipy"""
-    num_qubits = 1
-    circuit = QubitCircuit(num_qubits)
-    circuit.add_gate("X", 0)
-    processor = LinearSpinChain(num_qubits)
-    compiler = SpinChainCompiler(num_qubits, processor.params)
-    compiler.args.update({"shape": shape, "num_samples": 100})
-    processor.load_circuit(circuit, compiler=compiler)
-    if shape == "rectangular":
-        processor.pulse_mode = "discrete"
-    else:
-        processor.pulse_mode = "continuous"
-    init_state = basis(2, 0)
-    num_result = processor.run_state(init_state).states[-1]
-    ideal_result = circuit.run(init_state)
-    ifid = 1 - fidelity(num_result, ideal_result)
-    assert(pytest.approx(ifid, abs=0.01) == 0)
+    "shape", list(_default_window_t_max.keys()))
+def test_pulse_shape(shape):
+    """Test different pulse shape functions"""
+    coeff, tlist = GateCompiler.generate_pulse_shape(
+        shape, 1001, maximum=1.0, area=1.0)
+    assert pytest.approx(coeff[500], 1.e-2) == 1 # max
+    result = integrate.trapz(coeff, tlist)
+    assert pytest.approx(result, rel=1.e-2) == 1 # area
