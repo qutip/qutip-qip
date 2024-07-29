bcba3f838444f184c8c7788a22e204632e12da68
gadhvirushiraj <gadhvirushiraj@gmail.com>
2024-07-30 02:22:36 +0530

    added control rendering with multiqubit gate

--- src/qutip_qip/circuit/textrenderer.py
+++ src/qutip_qip/circuit/textrenderer.py
@@ -33,6 +33,8 @@ class TextRenderer:
             self._render_strs["mid"][i] = (
                 f"q{i} :" + self._render_strs["mid"][i]
             )
+            self._render_strs["top"][i] = "    " + self._render_strs["top"][i]
+            self._render_strs["bot"][i] = "    " + self._render_strs["bot"][i]
 
         self._layer_list = {
             i: [len(self._render_strs["mid"][i])] for i in range(self._qwires)
@@ -49,7 +51,6 @@ class TextRenderer:
         for i in range(self._qwires):
             if length_wire[i] < max_length:
                 diff = max_length - length_wire[i]
-
                 self._render_strs["top"][i] += " " * diff
                 self._render_strs["mid"][i] += "─" * diff
                 self._render_strs["bot"][i] += " " * diff
@@ -96,6 +97,7 @@ class TextRenderer:
 
         return top, mid, bot, len(top)
 
+    # TODO: sort this as well
     def draw_multiq_gate(self, gate):
         """
         Draw a multi qubit gate
@@ -116,6 +118,7 @@ class TextRenderer:
         # if control node is in wire below bot wire
         # place "┬" in the center of bot
 
+        # TODO fix: what if the control nodes are on both sides
         if gate.controls is not None:
             control_wire = gate.controls[0]
             if control_wire < gate.targets[0]:
@@ -134,6 +137,22 @@ class TextRenderer:
             print(self._render_strs["top"][i])
             print(self._render_strs["mid"][i])
             print(self._render_strs["bot"][i])
+
+    def adjust_layer(self, xskip, wire_list):
+        """
+        Adjust the layer
+        """
+
+        for wire in wire_list:
+            self._render_strs["top"][wire] += " " * (
+                xskip - len(self._render_strs["top"][wire])
+            )
+            self._render_strs["mid"][wire] += "─" * (
+                xskip - len(self._render_strs["mid"][wire])
+            )
+            self._render_strs["bot"][wire] += " " * (
+                xskip - len(self._render_strs["bot"][wire])
+            )
 
     def layout(self):
         """
@@ -157,90 +176,115 @@ class TextRenderer:
                 self.manage_layers(wire_list, layer, xskip, width)
 
                 for i, wire in enumerate(wire_list):
-                    self._render_strs["top"][wire] += " " * (
-                        xskip - len(self._render_strs["top"][wire])
-                    )
-                    self._render_strs["bot"][wire] += " " * (
-                        xskip - len(self._render_strs["bot"][wire])
-                    )
                     self._render_strs["top"][wire] += top
                     self._render_strs["mid"][wire] += mid
                     self._render_strs["bot"][wire] += bot
 
             else:
 
+                # TODO sort this out !!!
                 merged_wire = gate.targets.copy()
                 if gate.controls is not None:
                     merged_wire += gate.controls
-                merged_wire = sorted(merged_wire)
 
+                merged_wire = sorted(merged_wire)
                 wire_list = list(range(merged_wire[0], merged_wire[-1] + 1))
                 layer = max([len(self._layer_list[i]) for i in wire_list])
-
                 xskip = self.get_xskip(wire_list, layer)
                 top, mid, connect, connect_label, bot, width = (
                     self.draw_multiq_gate(gate)
                 )
                 self.manage_layers(wire_list, layer, xskip, width)
 
-                for i, wire in enumerate(wire_list):
+                # loop thorugh target
+                sorted_targets = sorted(gate.targets)
+                wire_list_target = list(
+                    range(sorted_targets[0], sorted_targets[-1] + 1)
+                )
 
-                    if i == 0:
-                        # add top as top and intr as mid
-                        self._render_strs["top"][wire] += " " * (
-                            xskip - len(self._render_strs["top"][wire])
-                        )
-                        self._render_strs["mid"][wire] += "─" * (
-                            xskip - len(self._render_strs["mid"][wire])
-                        )
-                        self._render_strs["bot"][wire] += " " * (
-                            xskip - len(self._render_strs["bot"][wire])
-                        )
+                self.adjust_layer(xskip, wire_list_target)
+                # main logic
+                for i, wire in enumerate(wire_list_target):
+
+                    if len(gate.targets) == 1:
                         self._render_strs["top"][wire] += top
-                        self._render_strs["bot"][wire] += mid
-
-                        if wire in gate.targets:
-                            self._render_strs["mid"][wire] += connect
-                        else:
-                            self._render_strs["mid"][wire] += mid
-
-                    elif i == len(wire_list) - 1:
-
-                        self._render_strs["top"][wire] += " " * (
-                            xskip - len(self._render_strs["top"][wire])
-                        )
-                        self._render_strs["mid"][wire] += "─" * (
-                            xskip - len(self._render_strs["mid"][wire])
-                        )
-                        self._render_strs["bot"][wire] += " " * (
-                            xskip - len(self._render_strs["bot"][wire])
-                        )
-                        self._render_strs["top"][wire] += mid
+                        self._render_strs["mid"][wire] += connect_label
                         self._render_strs["bot"][wire] += bot
-
-                        if wire in gate.targets:
-                            self._render_strs["mid"][wire] += connect_label
-                        else:
-                            self._render_strs["mid"][wire] += mid
-
-                    else:
-
-                        self._render_strs["top"][wire] += " " * (
-                            xskip - len(self._render_strs["top"][wire])
-                        )
-                        self._render_strs["mid"][wire] += "─" * (
-                            xskip - len(self._render_strs["mid"][wire])
-                        )
-                        self._render_strs["bot"][wire] += " " * (
-                            xskip - len(self._render_strs["bot"][wire])
-                        )
+                    elif i == 0 and wire in gate.targets:
+                        self._render_strs["top"][wire] += top
+                        self._render_strs["mid"][wire] += connect
+                        self._render_strs["bot"][wire] += mid
+                    elif (
+                        i == len(wire_list_target) - 1 and wire in gate.targets
+                    ):
                         self._render_strs["top"][wire] += mid
+                        self._render_strs["mid"][wire] += connect_label
+                        self._render_strs["bot"][wire] += bot
+                    else:
+                        self._render_strs["top"][wire] += mid
+                        self._render_strs["mid"][wire] += mid
                         self._render_strs["bot"][wire] += mid
 
-                        if wire in gate.targets:
-                            self._render_strs["mid"][wire] += connect
-                        else:
-                            self._render_strs["mid"][wire] += mid
+                # find if top or bot wire is closer to control wire
+                if gate.controls is not None:
+
+                    sorted_controls = sorted(gate.controls)
+                    if wire_list_target[0] < sorted_controls[0]:
+                        closest_pos = wire_list_target[0]
+                        is_top_closer = True
+                    else:
+                        closest_pos = wire_list_target[-1]
+                        is_top_closer = False
+
+                    # loop through control
+                    wire_list_control = list(
+                        range(
+                            min(sorted_controls[0], closest_pos),
+                            max(sorted_controls[-1], closest_pos) + 1,
+                        )
+                    )
+
+                    self.adjust_layer(xskip, wire_list_control)
+                    for i, wire in enumerate(wire_list_control):
+
+                        if wire not in gate.targets:
+                            if wire in gate.controls:
+                                
+                                self._render_strs["mid"][wire] += (
+                                    "─" * (width // 2)
+                                    + "▇"
+                                    + "─" * (width // 2)
+                                )
+                                self._render_strs["top"][wire] += (
+                                    " " * (width // 2)
+                                    + "|"
+                                    + " " * (width // 2)
+                                    if is_top_closer
+                                    else " " * width
+                                )
+                                self._render_strs["bot"][wire] += (
+                                    " " * (width // 2)
+                                    + "|"
+                                    + " " * (width // 2)
+                                    if not is_top_closer
+                                    else " " * width
+                                )
+                            else:
+                                self._render_strs["top"][wire] += (
+                                    " " * (width // 2)
+                                    + "|"
+                                    + " " * (width // 2)
+                                )
+                                self._render_strs["mid"][wire] += (
+                                    " " * (width // 2)
+                                    + "|"
+                                    + " " * (width // 2)
+                                )
+                                self._render_strs["bot"][wire] += (
+                                    " " * (width // 2)
+                                    + "|"
+                                    + " " * (width // 2)
+                                )
 
         self.extend_line()
         self.print_circuit()
