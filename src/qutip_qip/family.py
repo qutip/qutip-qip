e607a620c306451b45d30feeed09013b9b04570a
Simon Cross <hodgestar@gmail.com>
2025-01-11 00:38:16 +0100

    Add a family entry point module.

---
+++ src/qutip_qip/family.py
@@ -0,0 +1,9 @@
+"""QuTiP family package entry point."""
+
+
+def about():
+    """Return information to include in qutip.about()."""
+    return "qutip-qip", [
+        "Moo: 1.5",
+        "Bar: 2.0",
+    ]
