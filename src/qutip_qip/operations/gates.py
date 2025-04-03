23eacac9cbb8a323163560813b8c5966d1456fad
Eric Giguere <eric.giguere@calculquebec.ca>
2025-04-03 15:41:23 -0400

    Ensure expand_operator output dtype

--- src/qutip_qip/operations/gates.py
+++ src/qutip_qip/operations/gates.py
@@ -1326,7 +1326,7 @@ def expand_operator(
     for i, ind in enumerate(rest_pos):
         new_order[ind] = rest_qubits[i]
     id_list = [identity(dims[i]) for i in rest_pos]
-    return tensor([oper] + id_list).permute(new_order)
+    return tensor([oper] + id_list).permute(new_order).to(dtype)
 
 
 def gate_sequence_product(
