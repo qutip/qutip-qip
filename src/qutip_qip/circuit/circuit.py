fb541a913d43b7e52fe3f1179bd3bbc475b9e6d3
Mayank Goel <mayank.goel447@gmail.com>
2026-06-23 13:11:49 +0530

    All for Op to be completely classical - AND, OR etc.

--- src/qutip_qip/circuit/circuit.py
+++ src/qutip_qip/circuit/circuit.py
@@ -255,7 +255,7 @@ class QubitCircuit:
     def add_op(
         self: Self,
         op: Op,
-        qreg: int | IntSequence,
+        qreg: int | IntSequence = (),
         creg: int | IntSequence = (),
     ):
         if type(qreg) is int:
